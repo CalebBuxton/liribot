eaba6a9a76ebe30931df2c0d3129a756e9019064
Caleb Buxton <TACMB@int.mcmaster.com>
2017-07-13 15:59:52 -0400

    spotify, movie, and twitter bots functional

--- keys.js
+++ keys.js
@@ -5,11 +5,11 @@ exports.twitterKeys = {
   consumer_secret: 'BoGHTueBEamB5AhjupNXy6YcI0WTRXgl7kF5RSn3dMyEvl3F3C',
   access_token_key: '63189132-g2VzlONZCis47kLpp8VRjlwZA5kn8QFGsxzKiwWx7',
   access_token_secret: 'JWPHWHM7aveipvVAO4fl0WtyhuhiRmgASBw7Dq2dRPILJ',
-}
+};
 
 exports.spotifyKeys = {
-	client_id = 'e682f1b09c9d4bb782b8d69e7274314b';
-	client_secret = 'b23a128c3b9f4fce803f75b20dd8603b'
-}
+	client_id: 'e682f1b09c9d4bb782b8d69e7274314b',
+	client_secret: 'b23a128c3b9f4fce803f75b20dd8603b'
+};
 
 exports.OMDBKeys = '40e9cece';
--- liri.js
+++ liri.js
@@ -1,21 +1,24 @@
 var keys = require('./keys.js');
 var Spotify = require('node-spotify-api');
 var Twitter = require('twitter');
+var request = require('request');
 var twitterKeys = keys.twitterKeys;
 var spotifyKeys = keys.spotifyKeys;
 var omdbKeys = keys.OMDBKeys;
+var input = process.argv[2];
 
-var input = process.argv[3];
+init();
 
 function init() {
+	console.log(input)
 	if (input === 'my-tweets') {
 		getTweets();
 	}
 	else if (input === 'spotify-this-song') {
-		getSong(process.argv[4]);
+		getSong(process.argv[3]);
 	}
 	else if (input === 'movie-this'){
-		getMovie(process.argv[4]);
+		getMovie(process.argv[3]);
 	}
 	else if (input === 'do-what-it-says') {
 		doIt();
@@ -44,7 +47,7 @@ function getTweets() {
 
 	//find username
 	var user = ' ';
-	client.get('account/verify_credentials', function(error, account, response) {
+	client.get('account/verify_credentials', function(err, account, response) {
 		if (err) {
 			console.log('Error occured: ' + err);
 		}
@@ -55,64 +58,86 @@ function getTweets() {
 	})
 
 	//get most recent 20 tweets from this user
-	var params = {screen_name: user, count: '20'};
-	client.get('statuses/user_timeline', params, function(error, tweets, response) {
+	var params = {screen_name: user, count: '20', include_entities: 'false'};
+	client.get('statuses/user_timeline', params, function(err, tweets, response) {
 		if (err) {
 			console.log('Error occured: ' + err);
 		}
 		else {
-			console.log(tweets)
+			console.log('done')
+
+			console.log('Most recent tweets from ' + user + ' :')
+			console.log(' ');
+			for (var i = 0; i < tweets.length; i++) {
+				console.log((i + 1) + '. ' + tweets[i].text)
+				console.log('----------------------')
+			}
 		}
 	});
 
 }
 
-function getSong(request) {
+function getSong(songTitle) {
 	var song = '';
-	if(!request) {
+	//Testing if user input a song to search
+	if(!songTitle) {
 		song = 'The Sign';
 	}
 	else {
-		song = request;
+		song = songTitle;
 	}
 
+	//new Spotify instance
 	var spotify = new Spotify({
 		id: spotifyKeys.client_id,
 		secret: spotifyKeys.client_secret
 	});
 
-	spotify.search({ type: 'track', query: song }, function(err, data) {
+	//Spotify API Query
+	spotify.search({ type: 'track', query: song, limit: '1' }, function(err, data) {
 		if (err) {
 			return console.log('Error occurred: ' + err);
 		}
-
-		console.log(data); 
+		console.log('Artist: ' + data.tracks.items[0].artists[0].name); 
+		console.log('Song Title: ' + data.tracks.items[0].name); 
+		if(!data.tracks.items[0].preview_url) {
+			console.log('No Preview Available')
+		}
+		else {
+		console.log('Preview: ' + data.tracks.items[0].preview_url); 
+		}
+		console.log('From the Album: ' + data.tracks.items[0].album.name); 
 	});
-
-// Artist(s)
-// The song's name
-// A preview link of the song from Spotify
-// The album that the song is from
 }
 
-function getMovie(request) {
+function getMovie(movieTitle) {
 	var movie = '';
-	if(!request) {
+
+	//Test to see if user input a movie title
+	if(!movieTitle) {
 		movie = 'Mr. Nobody';
 	}
 	else {
-		movie = request;
+		movie = movieTitle;
 	}
 
-// * Title of the movie.
-// * Year the movie came out.
-// * IMDB Rating of the movie.
-// * Rotten Tomatoes Rating of the movie.
-// * Country where the movie was produced.
-// * Language of the movie.
-// * Plot of the movie.
-// * Actors in the movie.
-
+	//API call to OMDB
+	request('http://www.omdbapi.com/?apikey=' + omdbKeys +'&t=' + movie, function (err, response, data){
+		if (err) {
+			return console.log('Error occurred: ' + err);
+		}
+		else {
+			var data = JSON.parse(data);
+			console.log(data.Title); 
+			console.log('Released in: ' + data.Year); 
+			console.log('IMDB Rating: ' + data.Ratings[0].Value); 
+			console.log('Rotten Tomatoes Rating: ' + data.Ratings[1].Value); 
+			console.log('Produced in: ' + data.Country); 
+			console.log('Language: ' + data.Language); 
+			console.log(data.Plot); 
+			console.log('Starring: ' + data.Actors); 
+		}
+	})
 }
 
 function doIt(){
