a8b734358414718c65e64fa6c54b497bc1bddfeb
Caleb Buxton <TACMB@int.mcmaster.com>
2017-07-13 12:33:21 -0400

    added some comments for ease of use

--- liri.js
+++ liri.js
@@ -8,41 +8,41 @@ var omdbKeys = keys.OMDBKeys;
 var input = process.argv[3];
 
 function init() {
-if (input === 'my-tweets') {
-	getTweets();
-}
-else if (input === 'spotify-this-song') {
-	getSong(process.argv[4]);
-}
-else if (input === 'movie-this'){
-	getMovie(process.argv[4]);
-}
-else if (input === 'do-what-it-says') {
-	doIt();
-}
-else {
-	console.log('You entered an invalid command you dope.');
-	console.log('How about yous give one of these a try:');
-	console.log('my-tweets');
-	console.log("spotify-this-song 'Song Name Here'");
-	console.log("movie-this 'Movie Name Here'");
-	console.log('do-what-it-says');
-	console.log('...................');
-	console.log('Or you can just give up and leave me alone.');
-}
-
-
+	if (input === 'my-tweets') {
+		getTweets();
+	}
+	else if (input === 'spotify-this-song') {
+		getSong(process.argv[4]);
+	}
+	else if (input === 'movie-this'){
+		getMovie(process.argv[4]);
+	}
+	else if (input === 'do-what-it-says') {
+		doIt();
+	}
+	else {
+		console.log('You entered an invalid command you dope.');
+		console.log('How about yous give one of these a try:');
+		console.log('my-tweets');
+		console.log("spotify-this-song 'Song Name Here'");
+		console.log("movie-this 'Movie Name Here'");
+		console.log('do-what-it-says');
+		console.log('...................');
+		console.log('Or you can just give up and leave me alone.');
+	}
 }
 
 
 function getTweets() {
+	//start new Twitter instance
 	var client = new Twitter({
-  		consumer_key: twitterKeys.consumer_key,
-  		consumer_secret: twitterKeys.consumer_secret,
-  		access_token_key: twitterKeys.access_token_key,
-  		access_token_secret: twitterKeys.access_token_secret
+		consumer_key: twitterKeys.consumer_key,
+		consumer_secret: twitterKeys.consumer_secret,
+		access_token_key: twitterKeys.access_token_key,
+		access_token_secret: twitterKeys.access_token_secret
 	});
 
+	//find username
 	var user = ' ';
 	client.get('account/verify_credentials', function(error, account, response) {
 		if (err) {
@@ -50,10 +50,11 @@ function getTweets() {
 		}
 		else {
 			user = account.screen_name;
-			console.log(user)
+			console.log('Getting tweets for: ' + user)
 		}
 	})
 
+	//get most recent 20 tweets from this user
 	var params = {screen_name: user, count: '20'};
 	client.get('statuses/user_timeline', params, function(error, tweets, response) {
 		if (err) {
@@ -76,17 +77,17 @@ function getSong(request) {
 	}
 
 	var spotify = new Spotify({
-  		id: spotifyKeys.client_id,
- 		secret: spotifyKeys.client_secret
+		id: spotifyKeys.client_id,
+		secret: spotifyKeys.client_secret
 	});
- 
+
 	spotify.search({ type: 'track', query: song }, function(err, data) {
-  		if (err) {
-    	return console.log('Error occurred: ' + err);
-  	}
- 
-console.log(data); 
-});
+		if (err) {
+			return console.log('Error occurred: ' + err);
+		}
+
+		console.log(data); 
+	});
 
 // Artist(s)
 // The song's name
